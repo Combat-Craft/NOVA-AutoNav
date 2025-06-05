449031791253d705d97948e641527a4b2e8ed5eb
M.T <bobnakhla@gmail.com>
2025-06-05 13:13:04 -0400

    uploaded GPS test node

---
+++ gps/gps_package/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'gps_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'gps_node = gps_package.gps_node:main',
+        ],
+    },
+)
